adbd42c8f9141d303c33c8638c8822d0949851f1
一闪一闪亮晶晶 <resetsix223@gmail.com>
2024-03-23 18:20:10 +0800

    feat: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useTitle } from "ahooks";
+import { Flex } from "antd";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useTitle("使用 ahooks 修改标题");
+
+    return <Flex justify="center">Hello Demo</Flex>;
 }
 
 export default App;
